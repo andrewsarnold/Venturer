7755a6105171c3f727eed3519567c4a544c3d9f1
Andrew Arnold <andrew.s.arnold@gmail.com>
2016-08-08 13:37:42 -0400

    Game class; fixed window size

--- Venturer.ConsoleApp/Program.cs
+++ Venturer.ConsoleApp/Program.cs
@@ -3,94 +3,103 @@ using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
 using Microsoft.Win32.SafeHandles;
+using Venturer.Core;
 
 namespace Venturer.ConsoleApp
 {
-    public static class Program
-    {
-        #region DllImports
+	public static class Program
+	{
+		#region DllImports
 
-        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        private static extern SafeFileHandle CreateFile(
-            string fileName,
-            [MarshalAs(UnmanagedType.U4)] uint fileAccess,
-            [MarshalAs(UnmanagedType.U4)] uint fileShare,
-            IntPtr securityAttributes,
-            [MarshalAs(UnmanagedType.U4)] FileMode creationDisposition,
-            [MarshalAs(UnmanagedType.U4)] int flags,
-            IntPtr template);
+		[DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+		private static extern SafeFileHandle CreateFile(
+			string fileName,
+			[MarshalAs(UnmanagedType.U4)] uint fileAccess,
+			[MarshalAs(UnmanagedType.U4)] uint fileShare,
+			IntPtr securityAttributes,
+			[MarshalAs(UnmanagedType.U4)] FileMode creationDisposition,
+			[MarshalAs(UnmanagedType.U4)] int flags,
+			IntPtr template);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
-        private static extern bool WriteConsoleOutput(
-            SafeFileHandle hConsoleOutput,
-            CharInfo[] lpBuffer,
-            Coord dwBufferSize,
-            Coord dwBufferCoord,
-            ref SmallRect lpWriteRegion);
+		[DllImport("kernel32.dll", SetLastError = true)]
+		private static extern bool WriteConsoleOutput(
+			SafeFileHandle hConsoleOutput,
+			CharInfo[] lpBuffer,
+			Coord dwBufferSize,
+			Coord dwBufferCoord,
+			ref SmallRect lpWriteRegion);
 
-        #endregion
+		#endregion
 
-        [STAThread]
-        public static void Main(string[] args)
-        {
-            SafeFileHandle h = CreateFile("CONOUT$", 0x40000000, 2, IntPtr.Zero, FileMode.Open, 0, IntPtr.Zero);
-            if (h.IsInvalid)
-            {
-                Console.WriteLine("Error initializing.");
-                Console.ReadKey();
-                return;
-            }
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			SafeFileHandle h = CreateFile("CONOUT$", 0x40000000, 2, IntPtr.Zero, FileMode.Open, 0, IntPtr.Zero);
+			if (h.IsInvalid)
+			{
+				Console.WriteLine("Error initializing.");
+				Console.ReadKey();
+				return;
+			}
 
-            Console.OutputEncoding = Encoding.UTF8;
-            for (var i = 20; i <= 10000; i++)
-            {
-                Console.WriteLine("{0}\t\\u{1}\t{2}",
-                    i,
-                    i.ToString("x4"),
-                    char.ConvertFromUtf32(i));
+			using (var game = new Game())
+			{
+				Console.SetWindowSize(Game.WindowWidth, Game.WindowHeight);
+				Console.SetBufferSize(Game.WindowWidth, Game.WindowHeight);
+				Console.Title = game.Title;
+				Console.OutputEncoding = Encoding.UTF8;
+				Console.CursorVisible = false;
 
-                if (i % 20 == 0)
-                {
-                    Console.ReadKey();
-                }
-            }
-            Console.ReadKey();
-        }
-    }
+				for (var i = 20; i <= 10000; i++)
+				{
+					Console.WriteLine("{0}\t\\u{1}\t{2}",
+						i,
+						i.ToString("x4"),
+						char.ConvertFromUtf32(i));
 
-    #region Structs
+					if (i % 20 == 0)
+					{
+						Console.ReadKey();
+					}
+				}
+				Console.ReadKey();
+			}
+		}
+	}
 
-    [StructLayout(LayoutKind.Sequential)]
-    public struct Coord
-    {
-        public short X;
-        public short Y;
-    };
+	#region Structs
 
-    [StructLayout(LayoutKind.Explicit)]
-    public struct CharUnion
-    {
-        [FieldOffset(0)]
-        public byte AsciiChar;
-    }
+	[StructLayout(LayoutKind.Sequential)]
+	public struct Coord
+	{
+		public short X;
+		public short Y;
+	};
 
-    [StructLayout(LayoutKind.Explicit)]
-    public struct CharInfo
-    {
-        [FieldOffset(0)]
-        public CharUnion Char;
-        [FieldOffset(2)]
-        public short Attributes;
-    }
+	[StructLayout(LayoutKind.Explicit)]
+	public struct CharUnion
+	{
+		[FieldOffset(0)]
+		public byte AsciiChar;
+	}
 
-    [StructLayout(LayoutKind.Sequential)]
-    public struct SmallRect
-    {
-        public short Left;
-        public short Top;
-        public short Right;
-        public short Bottom;
-    }
+	[StructLayout(LayoutKind.Explicit)]
+	public struct CharInfo
+	{
+		[FieldOffset(0)]
+		public CharUnion Char;
+		[FieldOffset(2)]
+		public short Attributes;
+	}
 
-    #endregion
+	[StructLayout(LayoutKind.Sequential)]
+	public struct SmallRect
+	{
+		public short Left;
+		public short Top;
+		public short Right;
+		public short Bottom;
+	}
+
+	#endregion
 }
---
+++ Venturer.Core/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Venturer.Core
+{
+	public class Game : IDisposable
+	{
+		public const int WindowWidth = 78;
+		public const int WindowHeight = 24;
+		public string Title { get { return "Venturer.Core"; } }
+
+		public void Dispose()
+		{
+			// Save and clean up here
+		}
+	}
+}
