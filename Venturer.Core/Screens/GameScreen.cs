ed42b6931996cc00f20bc33f636d4a1f09da1297
Andrew Arnold <andrew.s.arnold@gmail.com>
2016-08-19 13:18:46 -0400

    Testing longer messages

--- Venturer.Core/Screens/GameScreen.cs
+++ Venturer.Core/Screens/GameScreen.cs
@@ -58,7 +58,13 @@ namespace Venturer.Core.Screens
 					_newScreen = PauseMenu;
 					break;
 				case Command.Misc:
-					_newScreen = new MultiTextScreen(new List<string> { "test 1", "test 2" });
+					_newScreen = new MultiTextScreen(new List<string>
+					{
+						"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+						"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+						"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+						"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+					});
 					break;
 			}
 
